de058ca39b9db7e66aee98c69e4498f731ac2a3a
OgataiKhan <badellino.kevin@gmail.com>
2023-11-23 15:52:21 +0100

    feat: bundle 3 debug

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -11,6 +11,7 @@
 
 
 // ESERCIZIO 1 (suggerimento: ci sono 6 errori)
+/*
 const cars = [
     {
         manufacturer: 'Ford',
@@ -72,6 +73,93 @@ const dieselCars = cars.filter( (auto) => {
 
 const otherCars = cars.filter( (auto) => {
     return auto.type !== 'benzina' || auto.type !== 'diesel';
+});
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
+*/
+
+// 1- Il codice divide un array di macchine per tipo di carburante, tra gasoline, diesel, e altri. Il risultato viene stampato in console.
+/* 2- Errori di sintassi:
+    1. Il tipo di carburante viene scritto a volte con la maiuscola e a volte con la minuscola. JS è case-sensitive. Fixo usando  toLowerCase(). In alternativa si potrebbe correggere i dati a mano, ma è molto meglio gestire il caso di dati con maiuscole sparse.
+    2. Manca una virgola dopo l'oggetto Seat Ibiza.
+    3. Se "(auto) >= auto.type === 'benzina'" ambisce ad essere una arrow function sarebbe utile scrivere bene la freccia. Sostituisco ">=" con "=>".
+*/
+/* 3- Errori logici:
+    1. La funzione in dieselCars viene scritta su più di una riga, quindi non ha il return implicito. La riscrivo in una riga sola.
+    2. In otherCars stiamo controllando se auto.type non è "benzina" OPPURE "diesel", che sarà sempre vero. Dovrebbe essere un AND.
+
+    Purtroppo non riesco a trovare il sesto errore se effettivamente presente.
+*/
+
+// Codice corretto:
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
+
+const dieselCars = cars.filter( (auto) => auto.type.toLowerCase() === 'diesel');
+
+const otherCars = cars.filter( (auto) => {
+    return auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel';
 });
 
 console.log('Auto a benzina');
